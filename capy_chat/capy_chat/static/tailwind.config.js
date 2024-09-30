016217c689ac057631fda4aedaa768ddf0b25153
Butterscotch Stallion <swift.plan5016@fastmail.com>
2024-09-29 21:27:43 -0400

    Updates tailwind config

--- capy_chat/capy_chat/static/tailwind.config.js
+++ capy_chat/capy_chat/static/tailwind.config.js
@@ -1,11 +1,22 @@
+const production = !process.env.ROLLUP_WATCH;
 export default {
     plugins: [],
     theme: {
         extend: {},
     },
-    purge: ["./index.html", './src/**/*.{svelte,js,ts}'], //for unused css
+    purge: {
+        content: [
+            "./src/**/*.svelte",
+
+        ],
+        enabled: production // disable purge in dev
+    },
     variants: {
         extend: {},
     },
     darkmode: true, // or 'media' or 'class'
+    future: {
+        purgeLayersByDefault: true,
+        removeDeprecatedGapUtilities: true,
+    },
 }
