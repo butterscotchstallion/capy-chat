dda733f92c236423b343cd2d270d0e224264bca3
Butterscotch Stallion <swift.plan5016@fastmail.com>
2024-10-06 15:41:02 -0400

    Refactor to implement default user creation

--- capy_chat/start_api.py
+++ capy_chat/start_api.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
-from sqlalchemy import select
-from sqlalchemy.orm import Session
 
 from capy_chat.api.lib.database import Base, engine
+from capy_chat.api.lib.db_setup import create_default_user
 from capy_chat.api.lib.logger import get_customized_logger
-from capy_chat.api.lib.models import User
 from capy_chat.api.routes import session_router, user_router, ws_router
 
 logger = get_customized_logger(__name__)
@@ -19,7 +17,4 @@ app.include_router(session_router)
 
 logger.info("Loaded main API")
 
-with Session(engine) as session:
-    stmt = select(User)
-    for user in session.scalars(stmt):
-        logger.info(user)
+create_default_user()
--- capy_chat/test/test_pw_utils.py
+++ capy_chat/test/test_pw_utils.py
@@ -1,12 +1,12 @@
 import os
 
-from capy_chat.api.lib.pw_utils import check_password, generate_password
+from capy_chat.api.lib.pw_utils import check_password, hash_password
 
 
 def test_check_password():
     salt = os.urandom(16)
     plaintext_password = "yumyumyum ice cream so good"
-    hashed_password = generate_password(plaintext_password, salt)
+    hashed_password = hash_password(plaintext_password, salt)
     assert check_password(
         plaintext_password, hashed_password, salt
     ), "Password mismatch"
