2f14db6327b198c7ac6ed9ca94f2c36c70153e94
Butterscotch Stallion <swift.plan5016@fastmail.com>
2024-11-03 11:46:03 -0500

    Moves sign on error outside grid

--- capy_chat/static/src/routes/sign-on/+page.svelte
+++ capy_chat/static/src/routes/sign-on/+page.svelte
@@ -1,14 +1,16 @@
 <script>
-	import SignInWindow from './signInWindow.svelte';
+    import SignInWindow from './signInWindow.svelte';
 
-	export let form;
+    export let form;
 </script>
 
 <main>
-	<div class="grid auto-cols-max grid-flow-col">
-		{#if form?.error}
-			<p class="error">{form.error}</p>
-		{/if}
-		<SignInWindow />
-	</div>
+    {#if form?.error}
+        <h4 class="text-2xl">Error signing on</h4>
+        <p class="error">{form.error}</p>
+    {/if}
+
+    <div class="grid auto-cols-max grid-flow-col">
+        <SignInWindow/>
+    </div>
 </main>
